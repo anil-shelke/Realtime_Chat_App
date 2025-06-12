a85133426b843993115d635216abc500ac9eba59
anil-shelke <142315275+anil-shelke@users.noreply.github.com>
2025-06-12 14:58:11 +0530

    __dirname and __filename is updated

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
 import cors from "cors"
 
+import { fileURLToPath } from 'url';
 import path from "path"
 
 import { connectDB } from "./lib/db.js";
@@ -13,7 +14,8 @@ import {app, server} from "./lib/socket.js"
 dotenv.config();
 
 const PORT = process.env.PORT
-const __dirname = path.resolve();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(express.json())
 app.use(cookieParser())
@@ -34,9 +36,9 @@ app.use("/api/messages",messageRoutes)
 if(process.env.NODE_ENV==="production"){
   app.use(express.static(path.join(__dirname, "../frontend/dist")))
   
-  app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
-  })
+ app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"))
+})
 }
 
 server.listen(PORT, () => {
